f479e6f36b4777e3750d9c0595ffdfdfba0a06d9
Karl Broman <kbroman@gmail.com>
2011-05-31 21:34:27 -0500

    Add readMat2.d (slight changes to readMat.d)

---
+++ IO/readMat2.d
@@ -0,0 +1,74 @@
+#!/usr/bin/rdmd
+/****************************************
+ * readMat2.d
+ * Karl Broman, 31 May 2011
+ * 
+ * read two matrices from text files and 
+ * calculate their product
+ * (slightly revised version of readMat.d)
+ ****************************************/
+
+import std.stdio;
+
+void main() {
+  auto Amat = readMatrix("A.txt");
+  auto Bmat = readMatrix("B.txt");
+
+  auto Cmat = matmult(Amat, Bmat);
+
+  writeln("\nresult:");
+  printMatrix(Cmat);
+}
+
+
+// read a matrix from a text file
+double [][]readMatrix(string filename)
+{
+  write("Reading from ", filename, ":");
+  auto f = File(filename, "r");
+  int ncol, nrow;
+
+  f.readf("%d %d\n", &nrow, &ncol);
+  writeln("    nrow=", nrow, " ncol=", ncol);
+  auto result = new double [][](nrow, ncol);
+  
+  foreach(i; 0 .. nrow) {
+    foreach(ref el; result[i]) {
+      f.readf("%s", &el);
+    }
+  }
+  return result;
+}
+
+
+// multiply two matrices
+double [][]matmult(double [][]Amat, double [][]Bmat)
+{
+  auto Cmat = new double[][](Amat.length, Bmat[0].length);
+
+  // should ensure that Bmat.length == Amat[0].length
+
+  foreach(i; 0 .. Amat.length) {
+    foreach(j; 0 .. Bmat[0].length) {
+      Cmat[i][j] = 0.0;
+      foreach(k; 0 .. Amat[0].length) {
+	Cmat[i][j] += Amat[i][k] * Bmat[k][j];
+      }
+    }
+  }
+  return Cmat;
+}
+
+
+// print a matrix
+void printMatrix(T)(T[][] matrix)
+{
+  foreach(i; 0 .. matrix.length) {
+    foreach(el; matrix[i]) {
+      writef("%s ", el);
+    }
+    write("\n");
+  }
+}
+
+// end of readMat2.d
