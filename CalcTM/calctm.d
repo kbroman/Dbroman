c9b7bd9fbd3229b4ad61a4615c2e91d2e5cbb2e9
Karl Broman <kbroman@gmail.com>
2011-06-08 13:56:40 -0500

    Modify calctm.d to handle X chr.
      - A lot of ugly stuff in here.  I should have started over.

--- CalcTM/calctm.d
+++ CalcTM/calctm.d
@@ -10,28 +10,41 @@ import std.stdio, std.conv, std.algorithm, std.regex;
 
 void main(string[] args) {
 
-  assert(args.length > 1, "Give no. strains");
+  assert(args.length > 2, "Give no. strains (2 or 4) and chromosome type (A or X).");
 
   int n_str = to!int(args[1]);
+  char chr_type = args[2][0];
 
   auto strains = ["A", "B", "C", "D"];
-  assert(n_str <= strains.length, "Can't use more than " ~ to!string(strains.length) ~ " strains");
-  assert(n_str % 2 == 0, "No. strains must be multiple of 2");
+  assert(n_str <= strains.length, "Can't use more than " ~ to!string(strains.length) ~ " strains.");
+  assert(n_str % 2 == 0, "No. strains must be multiple of 2.");
+  assert(chr_type == 'A' || chr_type == 'X', "Chromosome type must be 'A' or 'X'.");
 
   strains = strains[0..n_str];
 
-  auto individuals = getPairs(strains, "");
+  string[] pairs;
+  if(chr_type == 'A') {
+    auto individuals = getPairs(strains, strains, "");
+    pairs = getPairs(individuals, individuals, "x");
+  }
+  else {
+    if(n_str==4) {
+      strains = strains[0 .. 3];
+      n_str = 3;
+    }
+    auto females = getPairs(strains, strains, "");
+    auto males = strains.dup;
+    pairs = getPairs(females, males, "x");
+  }
 
-  auto pairs = getPairs(individuals, "x");
-
-  auto pairLookup = getPairLookup(pairs);
+  auto pairLookup = getPairLookup(pairs, chr_type, n_str);
 
   auto prototypes = getPrototypes(pairLookup);
   prototypes = prototypes.sort;
 
-  auto transitionMatrix = getTM(pairLookup, prototypes);
+  auto transitionMatrix = getTM(pairLookup, prototypes, chr_type);
 
-  if(args.length > 2 && args[2] == "maxima") {
+  if(args.length > 3 && args[3] == "maxima") {
     writeTMmaxima(transitionMatrix, prototypes, "P" ~ to!string(n_str));
   }
   else {
@@ -40,14 +53,14 @@ void main(string[] args) {
 }
 
 
-string[] getPairs(string[] symbols, string sep)
+string[] getPairs(string[] symbols_left, string[] symbols_right, string sep)
 {
-  auto n_pair = symbols.length^^2;
+  auto n_pair = symbols_left.length*symbols_right.length;
   auto pairs = new string[n_pair];
 
   int k = 0;
-  foreach(i; symbols) {
-    foreach(j; symbols) {
+  foreach(i; symbols_left) {
+    foreach(j; symbols_right) {
       pairs[k] = i ~ sep ~ j;
       k++;
     }
@@ -57,23 +70,25 @@ string[] getPairs(string[] symbols, string sep)
 }
 
 unittest {
-  assert(getPairs(["A"], "") == ["AA"]);
-  assert(getPairs(["A", "B"], "") == ["AA", "AB", "BA", "BB"]);
+  assert(getPairs(["A"], ["A"], "") == ["AA"]);
+  assert(getPairs(["A", "B"], ["A", "B"], "") == ["AA", "AB", "BA", "BB"]);
 
-  assert(getPairs(["A"], "x") == ["AxA"]);
-  assert(getPairs(["A", "B"], "x") == ["AxA", "AxB", "BxA", "BxB"]);
+  assert(getPairs(["A"], ["A"], "x") == ["AxA"]);
+  assert(getPairs(["A", "B"], ["A", "B"], "x") == ["AxA", "AxB", "BxA", "BxB"]);
 
-  assert(getPairs(["AA"], "x") == ["AAxAA"]);
-  assert(getPairs(["AA", "AB", "BB"], "x") == 
+  assert(getPairs(["AA"], ["AA"], "x") == ["AAxAA"]);
+  assert(getPairs(["AA", "AB", "BB"], ["AA", "AB", "BB"], "x") == 
 	 ["AAxAA", "AAxAB", "AAxBB", "ABxAA", "ABxAB", "ABxBB", "BBxAA", "BBxAB", "BBxBB"]);
+  assert(getPairs(["AA", "AB", "BB"], ["A", "B"], "x") == 
+	 ["AAxA", "AAxB", "ABxA", "ABxB", "BBxA", "BBxB"]);
 }
 
 
-string[] getOffspring(string parentpair)
+string[] getOffspringA(string parentpair) 
 {
   auto individuals = split(parentpair, regex("x"));
 
-  auto offspring = new string[4];
+  auto offspring = new string[individuals[0].length * individuals[1].length];
 
   int k = 0;
   foreach(i; individuals[0]) {
@@ -82,14 +97,43 @@ string[] getOffspring(string parentpair)
       k++;
     }
   }
+
   return offspring;
 }
 
 unittest {
-  assert(getOffspring("AAxAA") == ["AA", "AA", "AA", "AA"]);
-  assert(getOffspring("AAxAB") == ["AA", "AB", "AA", "AB"]);
-  assert(getOffspring("ABxCD") == ["AC", "AD", "BC", "BD"]);
+  assert(getOffspringA("AAxAA") == ["AA", "AA", "AA", "AA"]);
+  assert(getOffspringA("AAxAB") == ["AA", "AB", "AA", "AB"]);
+  assert(getOffspringA("ABxCD") == ["AC", "AD", "BC", "BD"]);
+  assert(getOffspringA("AAxA") == ["AA", "AA"]);
+  assert(getOffspringA("AAxB") == ["AB", "AB"]);
+  assert(getOffspringA("ABxC") == ["AC", "BC"]);
 }  
+
+
+string[] getOffspringXmale(string parentpair)
+{
+  auto individuals = split(parentpair, regex("x"));
+
+  auto offspring = new string[individuals[0].length];
+
+  int k = 0;
+  foreach(i; individuals[0]) {
+    offspring[k] = [i];
+    k++;
+  }
+
+  return offspring;
+}
+
+unittest {
+  assert(getOffspringXmale("AAxA") == ["A", "A"]);
+  assert(getOffspringXmale("AAxB") == ["A", "A"]);
+  assert(getOffspringXmale("ABxC") == ["A", "B"]);
+  assert(getOffspringXmale("ABxA") == ["A", "B"]);
+}  
+
+
 
 string switchPair(string parentpair) 
 {
@@ -121,6 +165,10 @@ unittest {
   assert(switchOneInd("AAxAB", 0) == "AAxAB");
   assert(switchOneInd("ABxCD", 0) == "BAxCD");
 
+  assert(switchOneInd("ABxA", 0) == "BAxA");
+  assert(switchOneInd("ABxC", 0) == "BAxC");
+  assert(switchOneInd("CDxB", 0) == "DCxB");
+
   assert(switchOneInd("AAxAA", 1) == "AAxAA");
   assert(switchOneInd("AAxAB", 1) == "AAxBA");
   assert(switchOneInd("ABxCD", 1) == "ABxDC");
@@ -146,70 +194,89 @@ unittest {
   assert(exchangeLetters("AAxAB", ['A':'B', 'B':'A']) == "BBxBA");
   assert(exchangeLetters("AAxAC", ['A':'C', 'C':'A']) == "CCxCA");
   assert(exchangeLetters("AAxAC", ['B':'C', 'C':'B']) == "AAxAB");
+
+  assert(exchangeLetters("AAxC", ['A':'B', 'B':'A']) == "BBxC");
+  assert(exchangeLetters("ACxB", ['A':'B', 'B':'A']) == "BCxA");
 }
 
 
-string[] getEquivPairs(string parentpair)
+string[] getEquivPairs(string parentpair, char chr_type, int n_str)
 {
   int[string] pairHash;
 
   pairHash[parentpair] = 1;
 
-  // switch pair
-   foreach(pair; pairHash.keys) {
-     pairHash[switchPair(pair)] = 1;
-   }
+  if(chr_type == 'A') {
+    // switch pair
+    foreach(pair; pairHash.keys) {
+      pairHash[switchPair(pair)] = 1;
+    }
+
+    // switch letters in second
+    foreach(pair; pairHash.keys) {
+      pairHash[switchOneInd(pair, 1)] = 1;
+    }
+  }
 
   // switch letters in first
   foreach(pair; pairHash.keys) {
     pairHash[switchOneInd(pair, 0)] = 1;
   }
 
-  // switch letters in second
-  foreach(pair; pairHash.keys) {
-    pairHash[switchOneInd(pair, 1)] = 1;
+  if(!(chr_type == 'X' && n_str == 2)) {
+    // switch A<->B
+    foreach(pair; pairHash.keys) {
+      pairHash[to!string(exchangeLetters(pair, ['A':'B', 'B':'A']))] = 1;
+    }
   }
 
-  // switch A<->B
-  foreach(pair; pairHash.keys) {
-    pairHash[to!string(exchangeLetters(pair, ['A':'B', 'B':'A']))] = 1;
-  }
+  if(chr_type == 'A' && n_str > 2) {
+    // switch C<->D
+    foreach(pair; pairHash.keys) {
+      pairHash[to!string(exchangeLetters(pair, ['C':'D', 'D':'C']))] = 1;
+    }
 
-  // switch C<->D
-  foreach(pair; pairHash.keys) {
-    pairHash[to!string(exchangeLetters(pair, ['C':'D', 'D':'C']))] = 1;
-  }
-
-  // switch A<->C, B<->D
-  foreach(pair; pairHash.keys) {
-    pairHash[to!string(exchangeLetters(pair, ['A':'C', 'C':'A', 'B':'D', 'D':'B']))] = 1;
+    // switch A<->C, B<->D
+    foreach(pair; pairHash.keys) {
+      pairHash[to!string(exchangeLetters(pair, ['A':'C', 'C':'A', 'B':'D', 'D':'B']))] = 1;
+    }
   }
   
   return pairHash.keys;
 }
 
 unittest {
-  assert(getEquivPairs("AAxAA").sort == ["AAxAA", "BBxBB", "CCxCC", "DDxDD"]);
-  assert(getEquivPairs("AAxAB").sort == ["AAxAB", "AAxBA", "ABxAA", "ABxBB", 
+  assert(getEquivPairs("AAxAA", 'A', 4).sort == ["AAxAA", "BBxBB", "CCxCC", "DDxDD"]);
+  assert(getEquivPairs("AAxAB", 'A', 4).sort == ["AAxAB", "AAxBA", "ABxAA", "ABxBB", 
 					 "BAxAA", "BAxBB", "BBxAB", "BBxBA",
 					 "CCxCD", "CCxDC", "CDxCC", "CDxDD",
 					 "DCxCC", "DCxDD", "DDxCD", "DDxDC"]);
+  assert(getEquivPairs("AAxAB", 'A', 4).sort == ["AAxAB", "AAxBA", "ABxAA", "ABxBB", 
+					 "BAxAA", "BAxBB", "BBxAB", "BBxBA",
+					 "CCxCD", "CCxDC", "CDxCC", "CDxDD",
+					 "DCxCC", "DCxDD", "DDxCD", "DDxDC"]);
+
+  assert(getEquivPairs("AAxA", 'X', 2).sort == ["AAxA"]);
+  assert(getEquivPairs("ABxA", 'X', 2).sort == ["ABxA","BAxA"]);
+  assert(getEquivPairs("ABxA", 'X', 4).sort == ["ABxA","ABxB", "BAxA","BAxB"]);
+  assert(getEquivPairs("ABxC", 'X', 4).sort == ["ABxC","BAxC"]);
+  assert(getEquivPairs("ACxA", 'X', 4).sort == ["ACxA","BCxB", "CAxA", "CBxB"]);
 }
 
 
-string[string] getPairLookup(string[] parentpairs) 
+string[string] getPairLookup(string[] parentpairs, char chr_type, int n_str) 
 {
   string[] equivPairs;
   string[string] seenPairs;
 
   foreach(pair; parentpairs) {
     if(pair in seenPairs) {
-      foreach(anEquivPair; getEquivPairs(pair)) {
+      foreach(anEquivPair; getEquivPairs(pair, chr_type, n_str)) {
 	seenPairs[anEquivPair] = seenPairs[pair];
       }
     }
     else {
-      foreach(anEquivPair; getEquivPairs(pair)) {
+      foreach(anEquivPair; getEquivPairs(pair, chr_type, n_str)) {
 	seenPairs[anEquivPair] = pair;
       }
     }
@@ -217,8 +284,6 @@ string[string] getPairLookup(string[] parentpairs)
 
   return seenPairs;
 }
-
-
 
 
 string[] getPrototypes(string[string] pairLookup)
@@ -237,14 +302,21 @@ unittest {
 }
 
 
-int[string][string] getTM(string[string] pairLookup, string[] prototypes)
+int[string][string] getTM(string[string] pairLookup, string[] prototypes, char chr_type)
 {
   int[string][string] result;
 
+  string[] pairs;
   foreach(prototype; prototypes) {
-    auto offspring = getOffspring(prototype);
-    auto pairs = getPairs(offspring, "x");
-    auto n_pairs = pairs.length;
+    if(chr_type == 'A') {
+      auto offspring = getOffspringA(prototype);
+      pairs = getPairs(offspring, offspring, "x");
+    }
+    else {
+      auto female_offspring = getOffspringA(prototype);
+      auto male_offspring = getOffspringXmale(prototype);
+      pairs = getPairs(female_offspring, male_offspring, "x");
+    }
 
     foreach(pair; pairs) {
       ++result[prototype][pairLookup[pair]];
@@ -256,9 +328,17 @@ int[string][string] getTM(string[string] pairLookup, string[] prototypes)
 
 void writeTM(int[string][string] tm, string[] prototypes)
 {
+  string gap;
+  if(prototypes[0].length==4) {
+    gap = " ";
+  }
+  else {
+    gap = "";
+  }
+
   write("        ");
   foreach(i; prototypes) {
-    write(i, "  ");
+    write(i, "  ", gap);
   }
   writeln();
 
@@ -290,15 +370,22 @@ void writeTMmaxima(int[string][string] tm, string[] prototypes, string label="P"
     rowSum += tm[prototypes[0]][i];
   }
 
+  string gap;
+  if(prototypes[0].length==4) {
+    gap = " ";
+  }
+  else {
+    gap = "";
+  }
+
   write("/*               ");
   foreach(i; prototypes) {
-    writef("  %s   ", i);
+    writef("  %s%s   ", i, gap);
   }
   writeln("*/");
 
-
   foreach(i; prototypes) {
-    write("/* ", i, " */    [ ");
+    write("/* ", i, gap, " */    [ ");
     foreach(j; prototypes) {
       if(j in tm[i]) {
 	writef("%4d/%-2d ", tm[i][j], rowSum);
