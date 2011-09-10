a5a86462fdaf19238b5e8f0d2afff0a6942f2d3d
Karl Broman <kbroman@gmail.com>
2011-09-10 05:22:34 +0200

    R function for calling calc_squares to test call from R to D with arrays

--- CallD/calc_square.R
+++ CallD/calc_square.R
@@ -11,3 +11,18 @@ function(x)
     as.double(x),
     z = as.double(0))$z
 }
+
+calc.squares <-
+function(x)
+{
+  file <- "calc_squares.so"
+  if(!is.loaded("R_calc_squares")) {
+    dyn.load(file)
+    cat(" -Loaded", file, "\n")
+  }
+
+ .C("R_calc_squares",
+    as.integer(length(x)),
+    as.double(x),
+    z = as.double(x))$z
+}
